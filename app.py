d6f076113d652959f4ed680d14932fcb82f45da3
Maxim Bespalov <antibody@yandex.ru>
2023-03-29 22:11:59 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, jsonify
+from threading import Thread
+import os
+
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+import sys
+
+def construct_index(directory_path):
+    # set maximum input size
+    max_input_size = 4096
+    # set number of output tokens
+    num_outputs = 2000
+    # set maximum chunk overlap
+    max_chunk_overlap = 20
+    # set chunk size limit
+    chunk_size_limit = 600
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo-0301", max_tokens=num_outputs))
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex(
+        documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
+    )
+
+    index.save_to_disk('index.json')
+
+    return index
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    query = request.form.get('query')
+    if not query:
+        return jsonify({'error': 'No query provided'}), 400
+
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(query, response_mode="compact")
+
+    if response.response is None:
+        return jsonify({'error': 'Chatbot could not generate a valid response'}), 500
+
+    return jsonify({'response': response.response})
+
+
+if __name__ == '__main__':
+    os.environ["OPENAI_API_KEY"] = "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"
+    construct_index("vnove")  # Comment out the threading line and call the function directly
+    app.run(debug=True, threaded=True, host='0.0.0.0',
+            port=int(os.environ.get('PORT', 8080)))
